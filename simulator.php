966c07f4b4f99f5d04b32e21f0bb5ea926120b95
Drew Atkinson <drewatkinson5@gmail.com>
2016-09-05 14:28:56 -0400

    removed old commeted out code in index.php and simulator.php

--- simulator.php
+++ simulator.php
@@ -3,15 +3,15 @@
   <head>
     <meta charset="utf-8">
     <title>SLAC Virtual Lab</title>
-    
+
     <meta name="description" content="">
     <meta name="author" content="">
-    
+
     <meta http-equiv="x-ua-compatible" content="IE=edge">
-    
+
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
-    
+
     <!-- favicon -->
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114-precomposed.png">
@@ -23,13 +23,13 @@
     <link href="bower_components/bootstrap-switch/dist/css/bootstrap2/bootstrap-switch.css" rel="stylesheet">
     <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
-    
+
     <!-- JavaScript Assets -->
     <!--<script src="bower_components/jquery/dist/jquery.min.js"></script>-->
-    
+
     <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-    
-    
+
+
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="bower_components/raphael/raphael-min.js"></script>
     <script src="bower_components/snap/snap.min.js"></script>
@@ -42,53 +42,6 @@
     <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
     <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 
-    <!-- MAKES THE MODAL APPEAR ON WINDOW LOAD (OLD DESIGN)-->
-    <!--<script type="text/javascript">
-      $(window).load(function () {
-        $('#choosePatientType').modal('show');
-      });
-    </script>-->
-  </head>
-
-  <!-- Connect to database -->
-
-  <?php
-    include 'php/connection.php';
-  ?>
-  
-  <body>
-    <!-- Popup for test type selection (OLD DESIGN) -->
-    <!-- <div class="modal fade" id="choosePatientType" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">
-              <span aria-hidden="true">&times;</span>
-              <span class="sr-only">
-                Close
-              </span>
-            </button>
-            &nbsp
-          </div>
-          <div class="modal-body">
-            <button type="button" class="btn btn-primary patientOptionPractice" data-toggle="btn" aria-pressed="false" autocomplete="false">
-              Practice Test
-            </button>
-            Name: <input type = "text" id = "name">
-            <input type ="submit" id ="name-submit" value ="Get Patient Info.">
-            
-            <button type="button" class="btn btn-primary patientOptionReal" data-toggle="btn" aria-pressed="false" autocomplete="false">
-              Real Test
-            </button>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> -->
 
     <!-- report findings modal -->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-backdrop="false">
@@ -143,7 +96,7 @@
                 //----------------------------------------------//
                 //-------pass all data to database here---------//
                 //----------------------------------------------//
-                
+
                 location.reload(); //reload page
               } else {
                   return false;
@@ -157,39 +110,6 @@
     </div>
 
 
-          <!-- Login/Register Modal MOVED TO HOMEPAGE -->
-
-  <!-- <div id="myModal1" class="modal fade" role="dialog">
-    <div class="modal-dialog"> -->
-
-      <!-- Modal content-->
-     <!--  <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Login Into Your Account</h4>
-        </div>
-        <div class="modal-body">
-          <form role="form">
-            <div class="form-group">
-              <label for="email">Email:</label>
-              <input type="email" class="form-control" id="email" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-              <label for="pwd">Password:</label>
-              <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-            </div>
-            <button type="submit" class="submit">Sign In</button>
-            <br><br><a href="templates/registration.html">Don't have an account? Sign Up!</a>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="submit" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-
-    </div>
-  </div> -->
-
     <!-- draggable modal for viewing plot for reference -->
     <script>$("#myModal").draggable({ handle: ".modal-header" });</script>
 
@@ -201,7 +121,7 @@
 
     <!--main interface-->
     <?php include("templates/main_interface.html"); ?>
-      
+
   </body>
   <!-- Global reference (variables shared between .js files)-->
   <script src="js/global.js"></script>
