c5c46dda2eb14e6568005c82c8e3942058ba16f5
Drew Atkinson <drewatkinson5@gmail.com>
2016-09-25 12:41:10 -0400

    add login functionality

--- simulator.php
+++ simulator.php
@@ -1,3 +1,11 @@
+<?php
+  session_start();
+  session_regenerate_id();
+  if(!isset($_SESSION['user']))      // if there is no valid session
+  {
+      header("Location: /index.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
