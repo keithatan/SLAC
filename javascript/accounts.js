e954d50b747d287f9e0aa0f656328ea7d1ba297a
Drew Atkinson <drewatkinson5@gmail.com>
2016-10-26 09:54:12 -0400

    improve registration using a jQuery post request

--- javascript/accounts.js
+++ javascript/accounts.js
@@ -1,166 +1,134 @@
-function register_valid(element)
-{
-	/*This function will test the registration page for valid responses
-	I used AJAX to show the user messages that describe the problem at hand
-	The last AJAX call adds the information to the database but also double checks the values
-	I was not able to terminate function before 3rd AJAX call for some reason so I had to double check values*/
+function register_valid(element) {
+  /*This function will test the registration page for valid responses
+  I used AJAX to show the user messages that describe the problem at hand
+  The last AJAX call adds the information to the database but also double checks the values
+  I was not able to terminate function before 3rd AJAX call for some reason so I had to double check values*/
 
 
-	/*var x = document.getElementById(element).elements;*/
-	var str = '';
-	var pattern;
-	var temp;
+  /*var x = document.getElementById(element).elements;*/
+  var str = '';
+  var pattern;
+  var temp;
 
-	var x = new Array(document.getElementById("gridRadios1"),
-		document.getElementById("gridRadios2"),
-		document.getElementById("FirstName"),
-		document.getElementById("LastName"),
-		document.getElementById("PUID"),
-		document.getElementById("Email"),
-		document.getElementById("Password"),
-		document.getElementById("Passwordconfirm"));
+  var x = new Array(document.getElementById("gridRadios1"),
+    document.getElementById("gridRadios2"),
+    document.getElementById("FirstName"),
+    document.getElementById("LastName"),
+    document.getElementById("PUID"),
+    document.getElementById("Email"),
+    document.getElementById("Password"),
+    document.getElementById("Passwordconfirm"));
 
-	//check all fields
-	for (var i = 7; i >= 2; i--)
-	{
-		if (x[i].value == "") {
-			alert("Please fill in all fields");
-			return false;
-		};
-	}
+  //check all fields
+  for (var i = 7; i >= 2; i--) {
+    if (x[i].value == "") {
+      alert("Please fill in all fields");
+      return false;
+    };
+  }
 
-	//check passwords to match
-	if (x[7].value != x[6].value)
-	{
-		alert("Passwords do not match!");
-		return false;
-	}
+  //check passwords to match
+  if (x[7].value != x[6].value) {
+    alert("Passwords do not match!");
+    return false;
+  }
 
-	//Check if email exists in database
-	str = "CheckEmail ";
-	pattern = /.*@purdue.edu/;
-	if (pattern.test(x[5].value))
-	{
-		str = str + x[5].value;
-	}
-	else
-	{
-		document.getElementById("CheckEmail").innerHTML = "Provide a Purdue Email";
-		return false;
-	}
-	/*FIRST AJAX CALL TO CHECK EMAIL*/
-	if (window.XMLHttpRequest)
-	{
-		xmlhttp = new XMLHttpRequest();
-	}
-	else
-	{
-		xmlhttp = new ActiveXObject();
-	}
+  //Check if email exists in database
+  str = "CheckEmail ";
+  pattern = /.*@purdue.edu/;
+  if (pattern.test(x[5].value)) {
+    str = str + x[5].value;
+  } else {
+    document.getElementById("CheckEmail").innerHTML = "Provide a Purdue Email";
+    return false;
+  }
+  /*FIRST AJAX CALL TO CHECK EMAIL*/
+  if (window.XMLHttpRequest) {
+    xmlhttp = new XMLHttpRequest();
+  } else {
+    xmlhttp = new ActiveXObject();
+  }
 
-	xmlhttp.onreadystatechange = function()
-	{
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			/*in registration.html there is a spot under Email to write an error message*/
-			document.getElementById("CheckEmail").innerHTML = xmlhttp.responseText;
-		}
-	}
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      /*in registration.html there is a spot under Email to write an error message*/
+      document.getElementById("CheckEmail").innerHTML = xmlhttp.responseText;
+    }
+  }
 
-	xmlhttp.open("GET", "../php/register_validation.php?q="+str, true);
-	xmlhttp.send();
+  xmlhttp.open("GET", "../php/register_validation.php?q=" + str, true);
+  xmlhttp.send();
 
-	//Check if PUID is 10 digits
-	str = "CheckPUID ";
-	pattern = /[0-9]{10}/;
-	if (pattern.test(x[4].value))
-	{
-		str = str + x[4].value;
-	}
-	else
-	{
-		document.getElementById("CheckPUID").innerHTML = "Provide a Valid PUID";
-		return false;
-	}
-	/*SECOND AJAX CALL TO CHECK PUID*/
-	if (window.XMLHttpRequest)
-	{
-		xmlhttp2 = new XMLHttpRequest();
-	}
-	else
-	{
-		xmlhttp2 = new ActiveXObject();
-	}
+  //Check if PUID is 10 digits
+  str = "CheckPUID ";
+  pattern = /[0-9]{10}/;
+  if (pattern.test(x[4].value)) {
+    str = str + x[4].value;
+  } else {
+    document.getElementById("CheckPUID").innerHTML = "Provide a Valid PUID";
+    return false;
+  }
+  /*SECOND AJAX CALL TO CHECK PUID*/
+  if (window.XMLHttpRequest) {
+    xmlhttp2 = new XMLHttpRequest();
+  } else {
+    xmlhttp2 = new ActiveXObject();
+  }
 
-	xmlhttp2.onreadystatechange = function()
-	{
-		if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
-			/*similar to how email error message is displayed*/
-			document.getElementById("CheckPUID").innerHTML = xmlhttp2.responseText;
-		}
-	}
+  xmlhttp2.onreadystatechange = function() {
+    if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
+      /*similar to how email error message is displayed*/
+      document.getElementById("CheckPUID").innerHTML = xmlhttp2.responseText;
+    }
+  }
 
-	xmlhttp2.open("GET", "../php/register_validation.php?q="+str, true);
-	xmlhttp2.send();
+  xmlhttp2.open("GET", "../php/register_validation.php?q=" + str, true);
+  xmlhttp2.send();
 
-	str = "";
-	if (x[0].checked)
-	{
-		str = "admin ";
-	}
-	else if (x[1].checked)
-	{
-		str = "student ";
-	}
-	else
-	{
-		alert("Need to select account type");
-		return false;
-	}
-	for (var i = 2; i < x.length; i++) {
+  register_data = {
+    firstname: x[2].value,
+    lastname: x[3].value,
+    PUID: x[4].value,
+    email: x[5].value,
+    pass: x[6].value
+  };
 
-		str = str + x[i].value + " ";
-	}
+  if (x[0].checked) {
+    register_data.admin = "admin";
+  } else if (x[1].checked) {
+    register_data.student = "student";
+  } else {
+    alert("Need to select account type");
+    return false;
+  }
 
-	/*THIRD AJAX CALL TO ADD TO DATABASE BUT ALSO CHECKS VALUES AGAIN*/
-	if (window.XMLHttpRequest)
-	{
-		xmlhttp3 = new XMLHttpRequest();
-	}
-	else
-	{
-		xmlhttp3 = new ActiveXObject();
-	}
+  $.post("/php/register.php", register_data, function(data) {
+    if (data) {
+      document.getElementById("SubmitResponse").innerHTML = data;
+    }
+  });
 
-	xmlhttp3.onreadystatechange = function()
-	{
-		if (xmlhttp3.readyState == 4 && xmlhttp3.status == 200) {
-			/*error message but could be made prettier im sure you guys can figure it out*/
-			document.getElementById("SubmitResponse").innerHTML = xmlhttp3.responseText;
-		}
-	}
-
-	xmlhttp3.open("GET", "../php/register.php?q="+str, true);
-	xmlhttp3.send();
-
-	return false;
+  return false;
 }
 
 /*This function will be used to verify login information and display error message if neccesary
 It follows the same format as the registration page for AJAX calls*/
-function login(element)
-{
-	var email = document.getElementById("email").value;
-	var password = document.getElementById("pwd").value;
-	var str = "";
+function login(element) {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pwd").value;
+  var str = "";
 
-	$.post( "/php/login.php", {email: email, pass: password}, function(data) {
-		if (data) {
-			console.log(data);
-			document.getElementById("LoginFail").innerHTML = data;
-		} else {
-			window.location.href = '/simulator.php';
-		}
-	});
+  $.post("/php/login.php", {
+    email: email,
+    pass: password
+  }, function(data) {
+    if (data) {
+      console.log(data);
+      document.getElementById("LoginFail").innerHTML = data;
+    } else {
+      window.location.href = '/simulator.php';
+    }
+  });
 
-	return false;
+  return false;
 }
--- php/register.php
+++ php/register.php
@@ -2,20 +2,17 @@
 	include "../php/connection.php";
 	/*AFTER all text field validations add user to database*/
 
-	$q = $_GET['q'];
-	$q = explode(" ", $q);
-
-	$admin = $q[0];
-	$firstname = $q[1];
-	$lastname = $q[2];
-	$PUID = $q[3];
-	$email = $q[4];
-	$password = $q[5];
+	$admin = $_POST["admin"];
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
+	$PUID = $_POST["PUID"];
+	$email = $_POST["email"];
+	$password = $_POST["pass"];
 
 	$hash_pass = password_hash($password, PASSWORD_DEFAULT);
 
 	//1 means admin 0 means student
-	if ($admin == 'admin') 
+	if ($admin == 'admin')
 	{
 		$user = 1;
 	}
@@ -26,7 +23,7 @@
 
 	//due to some stupid reason I have to check if email is used or not twice and display error message
 	$sql = "SELECT * from Users where email=" . "'". $email."'";
-	if ($res = $conn->query($sql)) 
+	if ($res = $conn->query($sql))
 	{
 		if($res->fetchColumn() > 0)
 		{
@@ -42,7 +39,7 @@
 
 	//due to some stupid reason I have to check if PUID is used or not twice and display error message
 	$sql = "SELECT * from Users where PUID=" . "'". $PUID."'";
-	if ($res = $conn->query($sql)) 
+	if ($res = $conn->query($sql))
 	{
 		if($res->fetchColumn() > 0)
 		{
@@ -57,7 +54,7 @@
 
 	//add the information to database finally
 	$sql = "INSERT INTO Users (account_type, first_name, last_name, PUID, email, PasswordHash) VALUES " . "('" . $user . "', '" . $firstname . "', '" . $lastname . "', '" . $PUID . "', '" . $email . "', '" . $hash_pass . "')";
-	if ($conn->query($sql)) 
+	if ($conn->query($sql))
 	{
 		echo "SUBMITTED! Please confirm email (currently not implemented)";
 		exit();
